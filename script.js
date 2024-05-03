fd6cf2ba5f2600087df70d882c21df818941ab91
Ferneko <fcgraciano@gmail.com>
2024-05-03 17:00:29 -0300

    Alterações

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function trocarPasso(passoAtual, proximoPasso)
 {
     window.location.href = proximoPasso;
 }
+
+function registrarInicio(){
+   
+        const dbParam = JSON.stringify(
+            {
+                "idade": document.querySelector("#Idade").value,
+                "comentario": "null",
+            }
+        );
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onload = function() {
+            localStorage.setItem("Id_Participante",xmlhttp.response)
+            console.log(xmlhttp.response)
+            //trocarPasso("inicio","passo1.html")
+        }
+        xmlhttp.open("POST", "https://etec.fernandograciano.com.br/participante.php");
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xmlhttp.send("dados="+dbParam );
+    }
