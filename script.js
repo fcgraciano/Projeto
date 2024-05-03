763ba4b520912888d553111703afdb9f567c5a84
Ferneko <fcgraciano@gmail.com>
2024-05-03 17:28:36 -0300

    metade dos passos

--- script.js
+++ script.js
@@ -17,6 +17,29 @@ function trocarPasso(passoAtual, proximoPasso)
     window.location.href = proximoPasso;
 }
 
+function registrarPasso(passoAtual, saida, proximoPasso)
+{
+    const dbParam = JSON.stringify(
+        {
+            "Id_Grupo": 1,
+            "Id_Participante": localStorage.getItem("Id_Participante"),
+            "DataHora": new Date(),
+            "Saida": saida,
+            "Passo": passo,
+            
+        }
+    );
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function() {
+        //localStorage.setItem("Id_Participante",xmlhttp.response)
+        //console.log(xmlhttp.response)
+        trocarPasso(passoAtual,proximoPasso)
+    }
+    xmlhttp.open("POST", "https://etec.fernandograciano.com.br/pesquisa.php");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("dados="+dbParam );
+}
+
 function registrarInicio(){
    
         const dbParam = JSON.stringify(
@@ -29,7 +52,7 @@ function registrarInicio(){
         xmlhttp.onload = function() {
             localStorage.setItem("Id_Participante",xmlhttp.response)
             console.log(xmlhttp.response)
-            //trocarPasso("inicio","passo1.html")
+            trocarPasso("inicio","passo1.html")
         }
         xmlhttp.open("POST", "https://etec.fernandograciano.com.br/participante.php");
         xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
